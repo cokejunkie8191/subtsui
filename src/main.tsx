3fdcf788bcf34726a5cf1d48b67ef32ee8031707
tsuyoshi.nakama <mohi.cocacola@gmail.com>
2026-05-07 05:06:40 +0900

    chore: remove v1 implementation, prepare for redesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 // src/main.tsx
 import React from 'react'
-import { render } from 'ink'
-import { App } from './app'
+import { render, Text } from 'ink'
 
-const { unmount } = render(<App />, { patchConsole: true })
-
-process.on('SIGTERM', () => { unmount(); process.exit(0) })
-process.on('SIGINT', () => { unmount(); process.exit(0) })
+render(<Text>SubTSUI starting...</Text>)
